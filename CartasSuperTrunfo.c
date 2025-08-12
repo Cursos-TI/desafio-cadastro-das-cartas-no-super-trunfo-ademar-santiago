dc4cb7c9cf39cf14f6b823ac43fd10db5e341661
Ademar Santiago <67010728+ademar-santiago@users.noreply.github.com>
2025-08-12 19:26:01 +0000

    Criei os cadastros das cartas 1 e 2.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,6 +8,51 @@ int main() {
     float area1, area2;
     float pib1, pib2;
     int pontos1, pontos2;
+
+    // Cadastro da Carta 1
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado1);
+
+    printf("Código (ex: A01): ");
+    scanf("%s", codigo1);
+
+    printf("Nome da cidade: ");
+    scanf(" %[^\n]", cidade1); // Lê até o final da linha, incluindo espaços
+
+    printf("População: ");
+    scanf("%d", &populacao1);
+
+    printf("Área (em km²): ");
+    scanf("%f", &area1);
+
+    printf("PIB (em bilhões de reais): ");
+    scanf("%f", &pib1);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos1);
+
+    // Cadastro da Carta 2
+    printf("\nEstado (A-H): ");
+    scanf(" %c", &estado2);
+
+    printf("Código (ex: A01): ");
+    scanf("%s", codigo2);
+
+    printf("Nome da cidade: ");
+    scanf(" %[^\n]", cidade2); // Lê até o final da linha, incluindo espaços
+
+    printf("População: ");
+    scanf("%d", &populacao2);
+
+    printf("Área (em km²): ");
+    scanf("%f", &area2);
+
+    printf("PIB (em bilhões de reais): ");
+    scanf("%f", &pib2);
+
+    printf("Número de pontos turísticos: ");
+    scanf("%d", &pontos2);
+
    
     return 0;
 }
