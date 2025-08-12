6fa8905ee2a53f88d822c6da7a350f75ea7747f4
Ademar Santiago <67010728+ademar-santiago@users.noreply.github.com>
2025-08-12 19:22:32 +0000

    Criei as funções.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    
+    char estado1, estado2;
+    char codigo1[4], codigo2[4];
+    char cidade1[50], cidade2[50];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontos1, pontos2;
    
     return 0;
 }
